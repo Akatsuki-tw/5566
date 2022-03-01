140e993298883a3fb823bcc0b30d41c3e474fb48
Akatsuki-tw <100186574+Akatsuki-tw@users.noreply.github.com>
2022-03-01 15:52:47 +0800

    fetch

---
+++ fetch_input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a ;
+    int b ;
+    int c ;
+    int d ;
+    int e ;
+
+    printf("請輸入五個數字，以空白隔開。\n");
+    (scanf("%d",&a));
+    (scanf("%d",&b));
+    (scanf("%d",&c));
+    (scanf("%d",&d));
+    (scanf("%d",&e));
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
